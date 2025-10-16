139bce29fd409d94eca56d233c3e4dac8b7cd3a2
Demonrichis <richisali453@gmail.com>
2025-10-16 17:45:37 +0530

    Implement welcome message on member join
    Updated the bot to send a welcome message when a member joins and added the bot token to run the bot.

--- bot.py
+++ bot.py
@@ -1,12 +1,9 @@
 import discord
 from discord.ext import commands
-
 bot = commands.Bot(command_prefix='Dso')  
 @bot.event
 async def on_member_join(member):
-    # Find the channel by name
     channel = discord.utils.get(member.guild.text_channels, name='demons_welcome') 
-    
     if channel is not None:  
         await channel.send(
             f"Welcome, {member.mention}, to {member.guild.name}! 🩸\n"
@@ -16,6 +13,4 @@ async def on_member_join(member):
             "📢 Stay updated: <#1382774081522237540>\n"
             "🏅 May the strongest rise and claim their glory!"
         )
-
-# Run the bot
-bot.run('')
+bot.run('MTQyNDQzOTcwOTk0NTMwMzE2Nw.GMN8S4.EmIy0UsF461cTs2KTjlOMdrC0WSIYUQHyFPUJM')
